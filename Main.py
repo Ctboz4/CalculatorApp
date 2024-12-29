c4a54cf7538983a46c2a7823cb8a4a8073fc92a4
Ctboz4 <Clayton.T.Boswell@outlook.com>
2024-12-29 14:21:30 -0600

    added a couple of buttons

--- Main.py
+++ Main.py
@@ -36,6 +36,25 @@ class Calculator:
     def DivisionSignDisplay(self):
         return "/"
     
+    def ClearButton(self, display):
+        display.config(state="normal")  # This makes the display editable, without this you cannot change the display at all
+        display.delete(0, 'end')       
+        display.config(state="readonly")  #This puts the display back to read only.... Make sure you always change the state back
+
+
+    # We need to make the actual buttons for each number 
+    def one_Button(self, number):
+        number.config(state="normal")
+        number.insert('end', "1")
+        number.config(state="readonly")
+    def two_Button(self, number):
+        number.config(state="normal")
+        number.insert('end', "2")
+        number.config(state="readonly")
+    
+
+#Call the class
+calculator = Calculator() 
     
     
         
@@ -49,6 +68,25 @@ display = Entry(root)
 display.grid(row=0, column=0, columnspan=4)
 display.config(state="readonly")
 
-display.pack()
+#Here I will put all of the Buttons, Make sure that you pass the "display" argument so that it knows where to put the number
+#We are going to use the grid system instead of the pack() method
+clear_button = Button(root, text="C", command=lambda: calculator.ClearButton(display))
+clear_button.grid(row=1, column=0)
+
+button_one = Button(root, text="1", command=lambda: calculator.one_Button(display))
+button_one.grid(row=1, column=1)
+
+button_two = Button(root, text="2", command=lambda: calculator.two_Button(display))
+button_two.grid(row=1,column=2)
+
+
+
+
+
+
+
+#Pack everything here
+
+
 
 root.mainloop()

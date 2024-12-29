4dec3a4b468726e15a30cbdc45bf84eeb928e097
Ctboz4 <Clayton.T.Boswell@outlook.com>
2024-12-29 09:47:50 -0600

    Added the functions

--- Main.py
+++ Main.py
@@ -1,1 +1,37 @@
-print("Hello World")
+from tkinter import *
+
+
+
+#I need to make a calculator class that holds all of the methods needed for a calculator
+class Calculator:
+    #This is the addition function
+    def Addition(self, a, b):
+        c = a + b
+        return c
+    
+    #this is the subtraction function
+    def Subtraction(self, a, b):
+        c = a - b
+        return c
+    #this is the multiplcation function
+    def Multiplcation(self, a, b):
+        c = a * b
+        return c
+    
+    #this is the division function
+    def Division(self, a, b):
+        #This will check if you try to divide by Zero
+        if a == 0 or b == 0:  
+            return "You cannot divide by 0!"
+        c = a / b
+        return c
+        
+
+
+
+calculator = Calculator()
+
+result = calculator.Multiplcation(6, 10)
+
+print(result)
+    

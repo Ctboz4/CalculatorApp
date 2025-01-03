d97b55960982179d2c6199e367953804c39baeed
Ctboz4 <Clayton.T.Boswell@outlook.com>
2025-01-02 20:39:50 -0600

    I am re-doing this project in Java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private double FirstNumberUsed;
+    private double SecondNumberUsed;
+    private String Operator;  
+
+    public Calculator(){
+        FirstNumberUsed = 0;
+        SecondNumberUsed = 0;
+        Operator = "";
+    }  
+}

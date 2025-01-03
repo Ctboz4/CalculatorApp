9b54ad984dac3eaddaf869f636b901784fd68e52
Ctboz4 <Clayton.T.Boswell@outlook.com>
2025-01-02 20:48:54 -0600

    added some stuff

--- App.java
+++ App.java
@@ -1,7 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class App{
     public static void main(String[] args){
         System.out.println("Hello World");
 
         Calculator calculator = new Calculator();
+
+        //Frame info
+        JFrame frame = new JFrame("Calculator");
+        frame.setVisible(true);//Without this it wont even show
+        frame.setSize(200,200);
+
+
+        //Button Info
+
+
     }
 }
